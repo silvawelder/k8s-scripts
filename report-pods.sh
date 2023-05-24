4fc2ad1192daac239d48fbcae909fa33c741dd44
silvawelder <58438567+silvawelder@users.noreply.github.com>
2023-05-24 16:17:36 -0300

    Update report-pods.sh

--- report-pods.sh
+++ report-pods.sh
@@ -9,10 +9,7 @@ cat ns-list.csv | while read NAME_SPACE;
 
 do
     echo "Running on namespace: $NAME_SPACE"
-
     echo "Setting up namespace:"
     kns $NAME_SPACE
     kubectl get pods |awk '{print $1 "\t" $3}' | awk -F '\t' '{sub(/-[^-]*-[^-]*$/, "", $1); print $1 ";" $2}' >> outputs/$NAME_SPACE.csv
-
-
 done
